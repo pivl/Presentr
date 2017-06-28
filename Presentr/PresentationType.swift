1fac9b1a332a20e31f1c7434b2cd2f355dee0384
Pavel Stasyuk <pivlyak@gmail.com>
2017-06-28 16:44:54 +0400

    Update PresentationType.swift
    Unable to build with Xcode 9.0 beta 2 (9M137d). Compile error: switch must be exhaustive

--- Presentr/PresentationType.swift
+++ Presentr/PresentationType.swift
@@ -42,7 +42,7 @@ public enum PresentationType {
             return (.full, .full)
         case .custom(let width, let height, _):
             return (width, height)
-        case .dynamic:
+        default:
             return nil
         }
     }
